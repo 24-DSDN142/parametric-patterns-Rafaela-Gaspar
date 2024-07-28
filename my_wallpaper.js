b67d1b5882d30bebf76c5800d29e85f6843e7b62
Rafaela-Gaspar <rafaelagaspar621@gmail.com>
2024-07-29 10:03:03 +1200

    my_wallpaper - Leaf V1
    Added in leaf.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -17,74 +17,105 @@ function wallpaper_background() {
 
 function my_symbol() {
 
+  // Leaf
+  draw_leaf(160, 50); // Originally 160, 50
 
+  // Taco
+  draw_taco(100, 50); // Originally 100, 50
+
+
+
+}
+
+function draw_leaf (LeafX, LeafY) {
+  // Leaf
+  fill(17, 97, 39); // Dark Green
+  noStroke();
+  ellipse(LeafX, LeafY, 20, 20);
+  beginShape();
+  vertex(LeafX - 8.5, LeafY + 5.5);
+  vertex(LeafX + 10, LeafY + 26);
+  vertex(LeafX + 10, LeafY);
+  endShape(CLOSE);
+
+  // Linework
+  stroke(8, 59, 48); // Darker Green
+  arc(LeafX, LeafY, 20, 20, 140, 0);
+  line(LeafX - 8.5, LeafY + 5.5, LeafX + 10, LeafY + 26);
+  line(LeafX + 10, LeafY + 26, LeafX + 10, LeafY);
+  noFill();
+  bezier(LeafX + 3, LeafY + 7, LeafX - 4, LeafY - 2, LeafX - 4, LeafY - 14, LeafX + 4, LeafY - 21);
+}
+
+function draw_taco(HeadX, HeadY) {
   // Beak
   fill(217, 114, 4); // Dark Orange
   stroke(163, 63, 0); // Darker Orange
-  ellipse(88, 52, 12, 11);
+  ellipse(HeadX - 12, HeadY + 2, 12, 11);
   fill(250, 169, 20); // Orange
-  vertex(90, 45);
-  quadraticVertex(78, 47, 80, 58);
-  quadraticVertex(84, 48, 100, 58);
+  beginShape();
+  vertex(HeadX - 10, HeadY - 5);
+  quadraticVertex(HeadX - 22, HeadY - 3, HeadX - 20, HeadY + 8);
+  quadraticVertex(HeadX - 16, HeadY - 2, HeadX, HeadY + 8);
   endShape(CLOSE);
 
   // Body
   fill(252, 219, 3); // Yellow
   noStroke();
   beginShape();
-  vertex(101, 91);
-  vertex(88, 53);
-  vertex(110.5, 43);
-  vertex(165, 106);
-  quadraticVertex(110, 100, 115, 70);
+  vertex(HeadX + 1, HeadY + 41);
+  vertex(HeadX - 12, HeadY + 3);
+  vertex(HeadX + 10.5, HeadY - 7);
+  vertex(HeadX + 65, HeadY + 56);
+  quadraticVertex(HeadX + 10, HeadY + 50, HeadX + 15, HeadY + 20);
   endShape(CLOSE);
 
   // Head, belly, tail ellipses
-  ellipse(100, 50, 25, 25);
-  ellipse(120, 85, 40, 40);
-  ellipse(138, 170, 10, 10);
+  ellipse(HeadX, HeadY, 25, 25);
+  ellipse(HeadX + 20, HeadY + 35, 40, 40);
+  ellipse(HeadX + 38, HeadY+ 120, 10, 10);
 
   // Tail
   beginShape();
-  vertex(133, 170);
-  vertex(123, 100);
-  vertex(143, 90);
-  vertex(143, 170);
+  vertex(HeadX + 33, HeadY + 120);
+  vertex(HeadX + 23, HeadY + 50);
+  vertex(HeadX + 43, HeadY + 40);
+  vertex(HeadX + 43, HeadY + 120);
   endShape(CLOSE);
 
   // Eyes
   fill(255); // White
   stroke(40);
-  ellipse(98.5, 49, 11, 11);
+  ellipse(HeadX - 1.5, HeadY - 1, 11, 11);
 
   fill(40); // Black
-  ellipse(98.5, 49, 6, 6);
+  ellipse(HeadX - 1.5, HeadY - 1, 6, 6);
 
   // Linework
   noFill();
   stroke(191, 140, 0);
   beginShape();
-  vertex(110.5, 43);
-  vertex(165, 106);
-  quadraticVertex(110, 100, 115, 70);
+  vertex(HeadX + 10.5, HeadY - 7);
+  vertex(HeadX + 65, HeadY + 56);
+  quadraticVertex(HeadX + 10, HeadY + 50, HeadX + 15, HeadY + 20);
   endShape();
-  arc(100, 50, 25, 25, 155, 323);
-  line(101, 91, 88, 53);
-  arc(120, 85, 40, 40, 80, 170);
-  line(133, 170, 124, 104.5);
-  arc(138, 170, 10, 10, 0, 180);
-  line(143, 102.5, 143, 170);
+  arc(HeadX, HeadY, 25, 25, 155, 323);
+  line(HeadX + 1, HeadY + 41, HeadX - 12, HeadY + 3);
+  arc(HeadX + 20, HeadY + 35, 40, 40, 80, 170);
+  line(HeadX + 33, HeadY + 120, HeadX + 24, HeadY + 54.5);
+  arc(HeadX + 38, HeadY + 120, 10, 10, 0, 180);
+  line(HeadX + 43, HeadY + 52.5, HeadX + 43, HeadY + 120);
 
   // Feet
   strokeWeight(3);
   stroke(217, 114, 4); // Dark Orange
-  line(120, 102, 114, 108);
-  line(120, 102, 118, 110);
-  line(110, 100, 104, 106);
-  line(110, 100, 108, 108);
+  line(HeadX + 20, HeadY + 52, HeadX + 14, HeadY + 58);
+  line(HeadX + 20, HeadY + 52, HeadX + 18, HeadY + 60);
+  line(HeadX + 10, HeadY + 50, HeadX + 4, HeadY + 56);
+  line(HeadX + 10, HeadY + 50, HeadX + 8, HeadY + 58);
 
   // Cheek Feathers
   stroke(23, 164, 207); // Blue
-  line(100, 60, 101, 63);
-  line(105, 58, 106.5, 60.5);
+  line(HeadX, HeadY + 10, HeadX + 1, HeadY + 13);
+  line(HeadX + 5, HeadY + 8, HeadX + 6.5, HeadY + 10.5);
 }
